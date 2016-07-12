a935dd5fcb18b96cb86c20eb4716e3bdf73c8b30
daniel-k-richardson <danielr9@utas.edu.au>
2016-07-12 21:04:52 +1000

    small formatting adjustments

--- bin-search-tree/bst.h
+++ bin-search-tree/bst.h
@@ -1,16 +1,13 @@
 #ifndef BST_H
 #define BST_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 
 typedef struct bstNode *BST;
 
 int number_of_elements_in_BST(BST self);
-
 BST add_element_to_BST(BST self, long student_id);
 BST delete_element_from_BST(BST self, long target);
 void inorder(BST self);
--- bin-search-tree/main.c
+++ bin-search-tree/main.c
@@ -8,26 +8,20 @@
 //  This is not perfect, I will be making improvements as I learn more.
 //  This is for a personal reference.
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "bst.h"
-
-
 
 int main(void)
 {
     // create the root node for the tree and a starting value.
     BST tree = NULL;
-    
     int r;
     
     for (int i = 0; i < 40; i++) {
         r = rand() % 100 + 1;
         tree = add_element_to_BST(tree, r);
     }
-    
     preorder(tree);
-   
+    destroy_tree(&tree);
     
     return EXIT_SUCCESS;
 }
